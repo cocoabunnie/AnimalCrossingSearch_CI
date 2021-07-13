051d3d721d9b740224724ed3a5485a0c1156a828
DuvivierBri <47341822+duvivierbri@users.noreply.github.com>
2021-07-13 14:51:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import axios from "axios";
-import { BrowserRouter, Switch, Route, Link  } from "react-router-dom";
+import { BrowserRouter, Route, Link  } from "react-router-dom";
 import './App.css';
 
 import Home from "./Components/Home";

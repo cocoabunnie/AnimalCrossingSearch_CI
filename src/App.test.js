120a49953f4860d5f582e3923b5a9391deb3a777
DuvivierBri <47341822+duvivierbri@users.noreply.github.com>
2021-07-13 14:45:22 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const title = "Animal";
+  const linkElement = screen.getByText(title);
   expect(linkElement).toBeInTheDocument();
 });
